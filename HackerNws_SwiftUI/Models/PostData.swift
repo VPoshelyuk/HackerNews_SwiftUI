9c117422e62375f74cb41142e4ed1dc9661fe336
VPoshelyuk <aslava198@gmail.com>
2020-04-02 14:42:43 -0400

    Done

---
+++ HackerNws_SwiftUI/Models/PostData.swift
@@ -0,0 +1,24 @@
+//
+//  PostData.swift
+//  HackerNws_SwiftUI
+//
+//  Created by Slava Pashaliuk on 4/2/20.
+//  Copyright © 2020 Viachaslau Pashaliuk. All rights reserved.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String?
+    
+    var id: String{
+        return objectID
+    }
+}
